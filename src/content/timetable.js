9bd9ceb1a485142005d69918c5577da9c35ae667
”sovsianykov” <swebproger@gmail.com>
2021-10-13 20:08:33 +0300

    timetable

---
+++ src/content/timetable.js
@@ -0,0 +1,38 @@
+export const weekDays = {
+  Monday: "Monday",
+  Tuesday: "Tuesday",
+  Wednesday: "Wednesday",
+  Thursday: "Thursday",
+  Friday: "Friday",
+  Saturday: "Saturday",
+  Sunday: "Sunday",
+};
+
+export const TIMETABLE = [
+  {
+    id: 1,
+    Monday: "Monday",
+    lesson_1: "ЯДС",
+    lesson_2: "Читання",
+    lesson_3: "Англ.мова",
+    lesson_4: "Письмо",
+    lesson_5: "Математика",
+  },
+  {
+    id: 2,
+    Tuesday: "Tuesday",
+    lesson_1: "ЯДС",
+    lesson_2: "Фiз.виховання",
+    lesson_3: "Письмо",
+    lesson_4: "Читання",
+    lesson_5: "Математика",
+  },
+  {
+    id: 3,
+    Tuesday: "Wednesday",
+    lesson_1: "ЯДС",
+    lesson_2: "Мистецтво",
+    lesson_3: "Англ.мова",
+    lesson_4: "Письмо",
+  },
+];
