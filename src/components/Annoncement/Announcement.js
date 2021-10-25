c1cada40583e5e9d4889996b17fbbfafc33c1023
”sovsianykov” <swebproger@gmail.com>
2021-10-25 11:00:35 +0300

    timetable well done

--- src/components/Annoncement/Announcement.js
+++ src/components/Annoncement/Announcement.js
@@ -11,7 +11,7 @@ const Announcement = ({ announcement }) => {
                   <Typography variant='h5' align='center' >
                       {announcement.title}
                   </Typography>
-                  <Typography variant='subtitle1' align='center' >
+                  <Typography variant='subtitle1' align='center' className={styles.message} >
                       {announcement.message}
                   </Typography>
               </Paper>
